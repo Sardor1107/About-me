8e589f2d6fd12b88c1b01aaf0668a2a080fcbdfb
Sardor1107 <chapayevsardor@gmail.com>
2023-01-14 00:26:19 +0500

    portfolio

---
+++ js/main.js
@@ -0,0 +1,36 @@
+window.addEventListener('DOMContentLoaded', function(){
+    window.addEventListener('scroll', function(){
+        const header = document.querySelector('.head');
+        header.classList.toggle('sticky', window.scrollY > 0)
+    })
+    const menuBtn = document.querySelector('.menu-btn')
+    const novigation = document.querySelector('.navigation')
+    const novigationItems = document.querySelectorAll('.link')
+    const scrollbtn = document.querySelector('.scrolltop-btn')
+    menuBtn.addEventListener('click', ()=>{
+        menuBtn.classList.toggle('active')
+        novigation.classList.toggle('active')
+    })
+    novigationItems.forEach(navitem => {
+        navitem.addEventListener('click', ()=>{
+            menuBtn.classList.remove('active')
+            novigation.classList.remove('active')
+        })
+    })
+    window.addEventListener('scroll', ()=>{
+        scrollbtn.classList.toggle('active', window.scrollY > 500)
+    })
+    scrollbtn.addEventListener('click', ()=>{
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    })
+
+
+    window.addEventListener('scroll', ()=>{
+        let reveals = document.querySelectorAll('.reveal')
+        for(let i = 0; i < reveals.length; i++) {
+            let windowHeight = window.innerHeight;
+            let revealtop = reveals[i].getBoundingClientRect().top
+            let revealpoint = 50
+    })
+})
